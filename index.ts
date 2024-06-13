6f4094cdc78c254f0850a6ac06a3c02d8ee423d9
RomanLugovskoy <romagma29@gmail.com>
2024-06-13 09:59:40 +0300

    e

--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
-const burgerType: string = 'big mac';
-console.log('Hello burgers!', burgerType);
+// const burgerType: string = 'big mac';
+// console.log('Hello burgers!', burgerType);
+// //----------------------
+// let exampleT: [string, number] = ['Diablo', 1];
+// //----------------------
+// function foo(param1: string, param2?: number) { 
+// 	console.log(param1 + param2);
+//   }
+//   // ? указывает опциональный аргумент
+//   foo('hello', 2020);
+//   foo('hello');
+//   foo('hello', 2022, 1991); //здесь будет ошибка
+
+// // !!опциональные аргументы всегда должны объявляться после обязательных — в противном случае будет ошибка!!
+
+// function foo(): void { 
+// 	console.log('Hello, SkillFactory!');
+//   }
+
+// // Тип void означает отсутствие конкретного типа. Он явно указывает, что у функции отсутствует возвращаемое значение
